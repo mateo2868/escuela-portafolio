aebf894ae08bb8e4b0126a22e2efb8802eb0e33a
Mateo vasquez Arango <mateovasqueza@gmail.com>
2025-09-09 14:15:55 -0500

    creacion de componenentes de inciio de sesion

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,18 +6,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursoComponent } from './pages/curso/curso.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 import { EstudianteComponent } from './pages/estudiante/estudiante.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CursoComponent,
-    EstudianteComponent
+    EstudianteComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    RouterModule,
     ReactiveFormsModule
   ],
   providers: [],
